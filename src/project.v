4fff0d6268b9ea2b1c3ef352bd97dfd016819ad3
sidhantm123 <sidhantmoola@gmail.com>
2026-03-31 23:36:42 -0400

    changed top module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module ttsky_analog_smoola (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
